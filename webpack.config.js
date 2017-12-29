b2c0e3cde0326ee7a55c84d8ce854392fd85433e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-29 14:40:51 -0500

    working bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,13 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
 const webpack = require('webpack')
 
-const plugins = [
-  new ExtractTextPlugin({
-    filename: './bundle.css',
-    allChunks: true,
-  }),
-  new webpack.optimize.ModuleConcatenationPlugin(),
-]
+const plugins = [new webpack.optimize.ModuleConcatenationPlugin()]
 
 module.exports = function webpackStuff(env) {
   if (env === 'production') plugins.push(new MinifyPlugin())
 
   return {
-    entry: ['./src/index.js', './styles/app.css'],
+    entry: ['./src/index.js'],
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, './'),
@@ -26,16 +20,10 @@ module.exports = function webpackStuff(env) {
           test: /\.js$/,
           loader: 'babel-loader',
           query: {
-            presets: ['es2015'],
+            presets: ['env'],
             plugins: [],
           },
           include: [path.resolve(__dirname, './')],
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            use: 'css-loader?importLoaders=1',
-          }),
         },
       ],
     },
