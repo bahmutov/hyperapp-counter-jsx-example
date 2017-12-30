6d777f1e1ecbe28676b6ef6cd85755a9af484264
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-30 12:13:35 -0500

    different counters

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 > Small counter example in Hyperapp and JSX (and Cypress tests)
 
-Webpack config taken from https://github.com/selfup/hyperapp-one
+You can mount your Hyperapp components in the tests using JSX syntax by transpiling it
+in the [cypress/plugins/index.js](cypress/plugins/index.js) file.
+
+```js
+import { mount } from 'cypress-hyperapp-unit-test'
+import { Counter } from '../../src/components/counter'
+it('calls onclick', () => {
+  const label = 'calls onclick'
+  const onclick = cy.spy()
+  // our component to test
+  const component = <Counter label={label} onclick={onclick} />
+  mount({}, {}, () => component)
+  cy
+    .contains(label)
+    .click()
+    .then(() => {
+      expect(onclick).to.have.been.calledOnce
+    })
+})
+```
+
+When running the test in Cypress you have full browser and can interact / inspect every test step by step.
+
+![Spec](images/counter-spec.png)
+
+## More info
+
+- webpack config taken from https://github.com/selfup/hyperapp-one
+- Hyperapp - Cypress bridge uses [cypress-hyperapp-unit-test](https://github.com/bahmutov/cypress-hyperapp-unit-test)
---
+++ cypress/integration/different-counters-spec.js
@@ -0,0 +1,70 @@
+// "h" while unused right now will be used when the
+// loader changes JSX syntax into "h" calls
+// eslint-disable-next-line no-unused-vars
+import { h } from 'hyperapp'
+import { mount } from 'cypress-hyperapp-unit-test'
+import { Counter } from '../../src/components/counter'
+
+// instantiate Counter inside each test, not in beforeEach
+
+/* eslint-env mocha */
+describe('Counter variations', () => {
+  it('calls onclick', () => {
+    const label = 'calls onclick'
+    const onclick = cy.spy()
+    const component = <Counter label={label} onclick={onclick} />
+    mount({}, {}, () => component)
+
+    cy
+      .contains(label)
+      .click()
+      .then(() => {
+        expect(onclick).to.have.been.calledOnce
+      })
+  })
+
+  it('can be inside another div', () => {
+    const label = 'calls onclick'
+    const onclick = cy.spy()
+    const component = (
+      <div>
+        <Counter label={label} onclick={onclick} />
+      </div>
+    )
+    mount({}, {}, () => component)
+
+    cy
+      .contains(label)
+      .click()
+      .then(() => {
+        expect(onclick).to.have.been.calledOnce
+      })
+  })
+
+  it('works even without onclick', () => {
+    const label = 'no click'
+    const component = (
+      <div>
+        <Counter label={label} />
+      </div>
+    )
+    mount({}, {}, () => component)
+    cy.contains(label)
+  })
+
+  it('works without label', () => {
+    const onclick = cy.spy()
+    const component = (
+      <div>
+        <Counter onclick={onclick} />
+      </div>
+    )
+    mount({}, {}, () => component)
+    cy
+      .get('button')
+      .click()
+      .then(() => {
+        expect(onclick).to.have.been.calledOnce
+      })
+  })
+})
