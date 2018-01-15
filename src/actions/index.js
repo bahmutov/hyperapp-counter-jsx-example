0e2915c9fddfa179c1d5d17cb3ffb8b996955916
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-15 11:04:50 -0500

    use Ramda lenses to work with state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,20 @@
+import { lensProp, over, inc, dec } from 'ramda'
+
+// typical code
+// export const actions = {
+//   down: () => state => ({ count: state.count - 1 }),
+//   up: () => state => ({ count: state.count + 1 })
+// }
+
+// using Ramda lenses
+// export const actions = {
+//   down: () => over(lensProp('count'), dec),
+//   up: () => over(lensProp('count'), inc)
+// }
+
+// reuse same lens logic
+const count = over(lensProp('count'))
 export const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 })
+  down: () => count(dec),
+  up: () => count(inc)
 }
